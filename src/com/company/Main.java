cac382c62b9f460f4622720cfe51a51b174903b8
nam0011 <nam0011@users.noreply.github.com>
2021-03-23 13:34:43 -0500

    build update
    updated for build to run -- this is port from 3/23/2021

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,14 +13,14 @@ public class Main {
     public static void main(String[] args)  {
 	// write your code here
 
-       // DemoSetup demoSetup = new DemoSetup();
+        DemoSetup demoSetup = new DemoSetup();
         DemoInventory demoinvent = new DemoInventory();
         demoinvent.testInventory();
 
-        //demoinvent.testChangeLogger();
+        demoinvent.testChangeLogger();
 
 
-       //new loginGUI();
+       new loginGUI();
 
 
 
