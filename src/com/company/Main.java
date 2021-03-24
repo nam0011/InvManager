ed8460cbfeca16d7ed4ac7026c13fbadf28e4d57
nam0011 <nam0011@users.noreply.github.com>
2021-03-24 11:47:42 -0500

    main fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,11 +13,11 @@ public class Main {
     public static void main(String[] args)  {
 	// write your code here
 
-       // DemoSetup demoSetup = new DemoSetup();
-       // DemoInventory demoinvent = new DemoInventory();
-       // demoinvent.testInventory();
+       DemoSetup demoSetup = new DemoSetup();
+       DemoInventory demoinvent = new DemoInventory();
+       demoinvent.testInventory();
 
-        //demoinvent.testChangeLogger();
+       demoinvent.testChangeLogger();
 
 
        new loginGUI();
